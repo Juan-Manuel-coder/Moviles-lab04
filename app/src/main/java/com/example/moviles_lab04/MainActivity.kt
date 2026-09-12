a3d8761331b233ac1f5c90718c594e23a4ee19ba
Juan-Manuel-coder <ramirezcondorijuanmanuel2@gmail.com>
2026-09-11 19:45:18 -0500

    feat: Add Card

--- app/src/main/java/com/example/moviles_lab04/MainActivity.kt
+++ app/src/main/java/com/example/moviles_lab04/MainActivity.kt
@@ -12,6 +12,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.moviles_lab04.ui.theme.Movileslab04Theme
+import androidx.compose.material3.Card
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.Column
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -32,10 +37,12 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Card(modifier = modifier.padding(16.dp)) {
+        Column(modifier = Modifier.padding(16.dp)) {
+            Text(text = "=== Card ===")
+            Text(text = "Hello $name!")
+        }
+    }
 }
 
 @Preview(showBackground = true)
